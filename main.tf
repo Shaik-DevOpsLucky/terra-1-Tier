8f8719f1e86418bb1b60c95c44c7a6152db615b8
Shaik-DevOpsLucky <115640750+Shaik-DevOpsLucky@users.noreply.github.com>
2024-03-29 13:29:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,81 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region  = var.region
 }
 
-resource "aws_instance" "one" {
-  ami                    = "ami-009e46eef82e25fef"
-  instance_type          = "t2.micro"
-  key_name               = "mumbai-keypair"
-  vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone      = "ap-south-1a"
-  user_data              = <<EOF
-#!/bin/bash
-sudo -i
-yum install httpd -y
-systemctl start httpd
-chkconfig httpd on
-echo "Hello world" > /var/www/html/index.html
-EOF
+resource "aws_instance" "demo-server" {
+  ami                          = var.os-name
+  key_name                     = var.key
+  instance_type                = var.instance-type
+  associate_public_ip_address  = true
+  subnet_id                    = aws_subnet.demo_subnet.id
+  vpc_security_group_ids       = [aws_security_group.demo-vpc-sg.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              echo "hai all this is my app created by terraform infrastructure by Moulali server-1" > /var/www/html/index.html
+              EOF
+
   tags = {
-    Name = "web-server-1"
+    Name = "demo-terraform-EC2"
   }
 }
 
+// Create VPC
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = var.vpc-cidr
 
-resource "aws_security_group" "five" {
-  name = "elb-sg"
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+// Create Subnet
+resource "aws_subnet" "demo_subnet" {
+  vpc_id            = aws_vpc.demo-vpc.id
+  cidr_block        = var.subnet1-cidr
+  availability_zone = var.subnet1-az
+
+  tags = {
+    Name = "demo-subnet"
+  }
+}
+
+// Create Internet Gateway
+resource "aws_internet_gateway" "demo-igw" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+    Name = "demo-igw"
+  }
+}
+
+// Create route table
+resource "aws_route_table" "demo-rtb" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo-igw.id
+  }
+
+  tags = {
+    Name = "demo-rtb"
+  }
+}
+
+// Associate subnet with route table
+resource "aws_route_table_association" "demo-rtb_association" {
+  subnet_id      = aws_subnet.demo_subnet.id
+  route_table_id = aws_route_table.demo-rtb.id
+}
+
+// Create a security group
+resource "aws_security_group" "demo-vpc-sg" {
+  name        = "demo-vpc-sg"
+  vpc_id      = aws_vpc.demo-vpc.id
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -44,5 +96,12 @@ resource "aws_security_group" "five" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow_http"
+  }
 }
-# continous integration build triggering when the code push into the scm
+
+resource "aws_s3_bucket" "six" {
+  bucket = "devopsbyshaikmoula3889900"
+}
